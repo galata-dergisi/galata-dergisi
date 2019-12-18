c5349263a90bc8578191f6eddfcad7c7f4465e89
Mehmet Baker <mehmet@mehmetbaker.dev>
2019-12-18 23:27:58 +0300

    Added license notices to source files
    Signed-off-by: Mehmet Baker <mehmet@mehmetbaker.dev>

--- client/src/components/Carousel.svelte
+++ client/src/components/Carousel.svelte
@@ -1,3 +1,22 @@
+ <!-- 
+   Copyright 2019 Mehmet Baker
+ 
+   This file is part of galata-dergisi.
+ 
+   galata-dergisi is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   
+   galata-dergisi is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with galata-dergisi. If not, see <https://www.gnu.org/licenses/>.
+  -->
+
 <script>
   import { onMount, onDestroy } from 'svelte';
   import Magazine from './Magazine.svelte';
