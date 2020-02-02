f680560d0fd2b12f321e2284f551942153479613
Mehmet Baker <mehmet@mehmetbaker.dev>
2020-02-02 19:17:38 +0300

    Working on magazine styling
    Signed-off-by: Mehmet Baker <mehmet@mehmetbaker.dev>

--- client/src/components/Magazine.svelte
+++ client/src/components/Magazine.svelte
@@ -47,9 +47,9 @@
     if (!magazineInstance.turn("hasPage", page)) {
       // Create an element for this page
       const element = jQuery("<div />", {
-        class: "page " + (page % 2 == 0 ? "odd" : "even"),
+        class: "page " + (page % 2 === 0 ? 'even' : 'odd'),
         id: "page-" + page,
-      }).html('<i class="loader"></i>');
+      }).html('<div class="loader"><div></div><div></div><div></div><div></div></div>');
 
       // If not then add the page
       magazineInstance.turn("addPage", element, page);
@@ -83,6 +83,8 @@
       pages: numberOfPages,
       elevation: 50,
       gradients: !jQuery.isTouch,
+      width: 1000,
+      height: 700,
       when: {
         turning: function(e, page, view) {
           // Gets the range of pages that the magazine needs right now
@@ -95,6 +97,12 @@
         turned: function(e, page) {
           moveLeft = page === 1 || page === numberOfPages;
         },
+        start: function () {
+          console.log('corner activated');
+        },
+        end: function () {
+          console.log('corner deactivated');
+        }
       },
     });
   });
@@ -121,6 +129,8 @@
   .center {
     position: absolute;
     left: calc((100% - 960px) / 2);
+    height: 720px;
+    padding-top: 20px;
   }
 
   .toolbar {
@@ -135,13 +145,107 @@
   .magazine {
     width: 960px;
     height: 700px;
-    transition: transform 0.5s ease;
+    transition: transform 0.5s ease, box-shadow .5s ease;
+    box-shadow: 0 0 20px #ccc;
   }
 
   .magazine.move-left {
     transform: translateX(-250px) !important;
+    box-shadow: none;
+  }
+
+  .magazine :global(.page) {
+    width:500px;
+	  height:700px;
+	  background:white;
+
+	  -webkit-box-shadow:0 0 20px rgba(0,0,0,0.2);
+	  -moz-box-shadow:0 0 20px rgba(0,0,0,0.2);
+	  -ms-box-shadow:0 0 20px rgba(0,0,0,0.2);
+	  -o-box-shadow:0 0 20px rgba(0,0,0,0.2);
+	  box-shadow:0 0 20px rgba(0,0,0,0.2);
+  }
+
+  .magazine :global(.odd .gradient) {
+    position:absolute;
+  	top:0;
+  	left:0;
+  	width:100%;
+  	height:100%;
+  	z-index:0;
+  	background:-webkit-gradient(linear, right top, left top, color-stop(0.95, rgba(0,0,0,0)), color-stop(1, rgba(0,0,0,0.15)));
+  	background-image:-webkit-linear-gradient(right,rgba(0,0,0,0) 95%, rgba(0,0,0,0.15) 100%);
+  	background-image:-moz-linear-gradient(right, rgba(0,0,0,0) 95%, rgba(0,0,0,0.15) 100%);
+  	background-image:-ms-linear-gradient(right, rgba(0,0,0,0) 95%, rgba(0,0,0,0.15) 100%);
+  	background-image:-o-linear-gradient(right, rgba(0,0,0,0) 95%, rgba(0,0,0,0.15) 100%);
+  	background-image:linear-gradient(right, rgba(0,0,0,0) 95%, rgba(0,0,0,0.15) 100%);
+  }
+
+  .magazine :global(.even .gradient) {
+  	position:absolute;
+  	top:0;
+  	left:0;
+  	width:100%;
+  	height:100%;
+  	z-index:0;
+  	background:-webkit-gradient(linear, left top, right top, color-stop(0.95, rgba(0,0,0,0)), color-stop(1, rgba(0,0,0,0.2)));
+  	background-image:-webkit-linear-gradient(left,rgba(0,0,0,0) 95%, rgba(0,0,0,0.2) 100%);
+  	background-image:-moz-linear-gradient(left, rgba(0,0,0,0) 95%, rgba(0,0,0,0.2) 100%);
+  	background-image:-ms-linear-gradient(left, rgba(0,0,0,0) 95%, rgba(0,0,0,0.2) 100%);
+  	background-image:-o-linear-gradient(left, rgba(0,0,0,0) 95%, rgba(0,0,0,0.2) 100%);
+  	background-image:linear-gradient(left, rgba(0,0,0,0) 95%, rgba(0,0,0,0.2) 100%);
+  }
+
+  .magazine :global(.zoom-in .gradient) {
+    display: none;
+  }
+
+  .magazine :global(.zoom-in .next-button, .zoom-in .previous-button) {
+    display: none;
+  }
+
+  .previous-button,.next-button {
+  	width: 22px;
+  	height: 700px;
+  	position: absolute;
+  	top: 0;
+  }
+
+  .next-button {
+  	right:-22px;
+  	-webkit-border-radius:0 15px 15px 0;
+  	-moz-border-radius:0 15px 15px 0;
+  	-ms-border-radius:0 15px 15px 0;
+  	-o-border-radius:0 15px 15px 0;
+  	border-radius:0 15px 15px 0;
+  }
+
+  .previous-button {
+  	left:-22px;
+  	-webkit-border-radius:15px 0 0 15px;
+  	-moz-border-radius:15px 0 0 15px;
+  	-ms-border-radius:15px 0 0 15px;
+  	-o-border-radius:15px 0 0 15px;
+  	border-radius:15px 0 0 15px;
+  }
+
+  .next-button:hover, .previous-button:hover {
+    background-color:rgba(0,0,0, 0.2);
   }
 </style>
+
+<div class="toolbar">
+  <a
+    href="/#"
+    title="İçindekiler"
+    on:click|preventDefault={() => goToPage(tableOfContents)}>
+    <i class="material-icons">list</i>
+  </a>
+
+  <a href="/#" title="Kapat" on:click|preventDefault={close}>
+    <i class="material-icons">cancel</i>
+  </a>
+</div>
 
 <div
   class="container"
@@ -150,19 +254,6 @@
   on:outroend
   bind:this={containerElement}>
   <div class="center">
-    <div class="toolbar">
-      <a
-        href="/#"
-        title="İçindekiler"
-        on:click|preventDefault={() => goToPage(tableOfContents)}>
-        <i class="material-icons">list</i>
-      </a>
-
-      <a href="/#" title="Kapat" on:click|preventDefault={close}>
-        <i class="material-icons">cancel</i>
-      </a>
-    </div>
-
     <div bind:this={magazine} class:move-left={moveLeft} class="magazine">
       <img
         src={thumbnailURL.replace('thumbnail', 'front')}
