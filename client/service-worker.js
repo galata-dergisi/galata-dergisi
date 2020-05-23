09c22aaf357492da5bab0a28caa443035c1fc38e
Mehmet Baker <mehmet@mehmetbaker.dev>
2020-05-23 20:48:03 +0300

    Fixed a minor layout issue
    Signed-off-by: Mehmet Baker <mehmet@mehmetbaker.dev>

--- client/service-worker.js
+++ client/service-worker.js
@@ -17,7 +17,7 @@
 
 /* eslint no-restricted-globals: 1 */
 
-const CACHE_NAME = 'galatadergisi-cache-v8';
+const CACHE_NAME = 'galatadergisi-cache-v9';
 const networkOnlyList = [
   /\/magazines\/sayi\d+\/audio\//,
 ];
