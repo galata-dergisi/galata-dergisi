067bce8be8f9f71690109b3636539d238c83a810
Mehmet Baker <mehmet@mehmetbaker.dev>
2020-06-07 23:34:27 +0300

    Contribution file input: dynamic accept criteria
    Closes #7
    
    Signed-off-by: Mehmet Baker <mehmet@mehmetbaker.dev>

--- client/service-worker.js
+++ client/service-worker.js
@@ -17,7 +17,7 @@
 
 /* eslint no-restricted-globals: 1 */
 
-const CACHE_NAME = 'galatadergisi-cache-v13';
+const CACHE_NAME = 'galatadergisi-cache-v14';
 const networkOnlyList = [
   /\/magazines\/sayi\d+\/audio\//,
 ];
