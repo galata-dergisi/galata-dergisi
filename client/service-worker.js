2ec7e545d63f4826cb6f97f20216f72254aeb28c
Mehmet Baker <mehmet@mehmetbaker.dev>
2021-05-12 00:03:23 +0300

    Updated cache name of the service worker
    Signed-off-by: Mehmet Baker <mehmet@mehmetbaker.dev>

--- client/service-worker.js
+++ client/service-worker.js
@@ -1,4 +1,4 @@
-// Copyright 2020 Mehmet Baker
+// Copyright 2020-2021 Mehmet Baker
 //
 // This file is part of galata-dergisi.
 //
@@ -17,7 +17,7 @@
 
 /* eslint no-restricted-globals: 1 */
 
-const CACHE_NAME = 'galatadergisi-cache-v17';
+const CACHE_NAME = 'galatadergisi-cache-v18';
 const networkOnlyList = [
   /\/magazines\/sayi\d+\/audio\//,
 ];
